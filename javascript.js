40c613641c30a8034b88e4adff7e014e45243d2f
SpookySpenc <115520984+SpencerB1994@users.noreply.github.com>
2023-02-27 13:30:51 -0700

    adjusted phone regex

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ function isFormValid(event) {
   const phone = document.querySelector("#phone");
   const email = document.querySelector("#email");
   const radio = document.querySelector("prefferedMethod");
-  const phoneRegex = /([A-Za-z0-9]+(-[A-Za-z0-9]+)+)/i;
+  const phoneRegex = ^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$
   const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
   const comments = document.querySelector("#comments");
  
